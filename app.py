3bf1ef9225d4c1b219817ed7a6f967fd1579fcb9
FiNkacho <sabaxuxunashvili@gmail.com>
2018-06-26 01:09:21 +0400

    Added stadium layout

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
 from tkinter import *
 
+w = 700
+h = 400
+
 root = Tk()
 root.resizable(False, False)
-root.geometry("{}x{}".format(700, 400))
+root.geometry("{}x{}".format(w, h))
 root.title("Ping-Pong")
 
+
+# Add canvas to screen
+c = Canvas(root, width=w, height=h, bg="#1050b7")
+c.pack()
+
+# Add stadium layout
+c.create_rectangle(0, 0, 15, h, fill="#fff", outline="#fff")
+c.create_rectangle(w-15, 0, w, h, fill="#fff", outline="#fff")
+c.create_rectangle(w//2-5, 0, w//2+5, h, fill="#fff", outline="#fff")
+
+ball = c.create_oval(w//2-20, h//2-20, w//2+20, h//2+20, fill="#EA9111", outline="#EA9111")
 
 root.mainloop()
